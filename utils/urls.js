93e334faf86d2f5b2e8eea66bdefe25d078614c7
Mariusz <urban.mariusz@icloud.com>
2021-12-07 18:31:17 +0100

    changed

--- utils/urls.js
+++ utils/urls.js
@@ -9,17 +9,18 @@ export const STRIPE_PK =
   "pk_test_51K2YaiJtB9uSPJxeV479keXvpfuSW71Sqx7420OtIf7HsMdluV8yoVfgaw5iXcaEUhu8Prgrm5jIvVWMBm8Wddhh00k19Sodxk";
 
 /**
- * Given an image return the URL
- * Works for local and deployed strapi
+ * Given a image object return the proper path to display it
+ * Provides a default as well
  * @param {any} image
  */
-
 export const fromImageToUrl = (image) => {
   if (!image) {
-    return "./vercel.svg";
+    return "/vercel.svg"; //Or default image here
   }
   if (image.url.indexOf("/") === 0) {
+    //It's a relative url, add API URL
     return `${API_URL}${image.url}`;
   }
+
   return image.url;
 };
